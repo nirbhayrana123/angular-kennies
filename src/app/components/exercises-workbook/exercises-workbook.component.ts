1c33174af26b00af8bc2e79608a45caf3c9c6b35
nirbhayrana123 <nirbhydesign@gmail.com>
2025-08-06 16:45:45 +0530

    commit amit

--- src/app/components/exercises-workbook/exercises-workbook.component.ts
+++ src/app/components/exercises-workbook/exercises-workbook.component.ts
@@ -1,14 +1,6 @@
 import {
-  AfterViewInit,
   Component,
-  ElementRef,
-  HostListener,
-  Inject,
-  PLATFORM_ID,
-  ViewChildren,
-  QueryList,
 } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-exercises-workbook',
@@ -16,39 +8,7 @@ import { isPlatformBrowser } from '@angular/common';
   templateUrl: './exercises-workbook.component.html',
   styleUrls: ['./exercises-workbook.component.css'],
 })
-export class ExercisesWorkbookComponent implements AfterViewInit {
-  @ViewChildren('stepElement') steps!: QueryList<ElementRef>;
-  isBrowser: boolean = false;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    this.isBrowser = isPlatformBrowser(platformId);
-  }
-
+export class ExercisesWorkbookComponent  {
   
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    if (!this.isBrowser) return;
 
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    const windowHeight = window.innerHeight;
-    const scrollMiddle = scrollTop + windowHeight / 2;
-
-    this.steps.forEach((step) => {
-      const el = step.nativeElement;
-      const offsetTop = el.offsetTop;
-      const offsetBottom = offsetTop + el.offsetHeight;
-
-      if (scrollMiddle >= offsetTop && scrollMiddle < offsetBottom) {
-        el.classList.add('active');
-      } else {
-        el.classList.remove('active');
-      }
-    });
-  }
-
-  ngAfterViewInit(): void {
-    if (this.isBrowser) {
-      this.onWindowScroll();
-    }
-  }
 }

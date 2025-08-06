b91d5dc6d498f1d35623d8dab33e59c1aea6bff9
nirbhayrana123 <nirbhydesign@gmail.com>
2025-08-06 15:52:52 +0530

    commit amit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/header/header/header.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({

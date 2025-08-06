53abfe54dc317cd3dde3e952045180adb95ecf71
nirbhayrana123 <nirbhydesign@gmail.com>
2025-08-06 14:21:22 +0530

    maneesh

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Title, Meta } from '@angular/platform-browser';
 import { EmotionalswiperComponent } from '../../components/emotionalswiper/emotionalswiper.component';
 import { CourseSliderComponent } from '../../components/course-slider/course-slider/course-slider.component';
+import * as AOS from 'aos';
 
 
 @Component({
@@ -12,7 +13,7 @@ import { CourseSliderComponent } from '../../components/course-slider/course-sli
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
 constructor(private titleService: Title, private metaService: Meta) {
     this.titleService.setTitle('Home - My Awesome Website');
@@ -22,5 +23,12 @@ constructor(private titleService: Title, private metaService: Meta) {
     });
   }
 
+
+    ngOnInit(): void {
+    AOS.init({
+      duration: 1200,
+      once: true,
+    });
+  }
 
 }

8697c2564ed27bc6aa2aabb800ac14319ff8dbae
nirbhayrana123 <nirbhydesign@gmail.com>
2025-08-07 17:38:37 +0530

    nirbhiya

--- src/app/layout/contact/contact.component.ts
+++ src/app/layout/contact/contact.component.ts
@@ -1,19 +1,60 @@
 import { Component } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
+import { WpService } from '../../services/wp.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // 👈 Import this
+import { NgIf } from '@angular/common'; // 👈 Optional if you prefer individual directive
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule, NgIf],
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']   
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-  constructor(private titleService: Title, private metaService: Meta) {
+  formData = {
+    name: '',
+    lastname: '',
+    email: '',
+    phone: '',
+    message: ''
+  };
+formSuccess = false;
+formSubmitted = false;
+
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+    private wpService: WpService
+  ) {
     this.titleService.setTitle('Contact Kenny Weiss - Kenny Weiss');
     this.metaService.updateTag({
       name: 'description',
       content: 'Face your pain and break free from self-destructive behaviors with a video response from Kenny, directed to you privately.',
+    });
+  }
+
+  submitForm(form :any) {
+     this.formSubmitted = true;
+    this.wpService.sendForm(this.formData).subscribe({
+      next: (res) => {
+        console.log('✅ Success:', res);
+         this.formSuccess = true; 
+        // alert('Message sent successfully!');
+        this.formData = {
+          name: '',
+          lastname: '',
+          email: '',
+          phone: '',
+          message: ''
+        };
+        this.formSubmitted = false;
+      },
+      error: (err) => {
+        console.error('❌ Error:', err);
+        alert('Failed to send message. Please try again.');
+      }
     });
   }
 }

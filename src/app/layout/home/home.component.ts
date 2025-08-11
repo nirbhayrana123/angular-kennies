8b19d4c8afe0722fef152d7f469b514c19a10363
nirbhayrana123 <nirbhydesign@gmail.com>
2025-08-11 12:26:21 +0530

    blog dynamic

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser,CommonModule } from '@angular/common';
 import { Title, Meta } from '@angular/platform-browser';
 
 import { EmotionalswiperComponent } from '../../components/emotionalswiper/emotionalswiper.component';
 import { CourseSliderComponent } from '../../components/course-slider/course-slider/course-slider.component';
 import { ExercisesWorkbookComponent } from '../../components/exercises-workbook/exercises-workbook.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';  
+import { WpService } from '../../services/wp.service';  
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [EmotionalswiperComponent, CourseSliderComponent, ExercisesWorkbookComponent, RouterModule],
+  imports: [EmotionalswiperComponent, CourseSliderComponent, ExercisesWorkbookComponent, RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
@@ -18,10 +19,12 @@ export class HomeComponent  {
 
   isExpanded = false;
   isBrowser: boolean;
-
+   posts: any[] = []; 
+   acfData: any; 
   constructor(
     private titleService: Title,
     private metaService: Meta,
+     private wp: WpService, 
     @Inject(PLATFORM_ID) private platformId: Object
   ) {
     this.isBrowser = isPlatformBrowser(platformId);
@@ -34,9 +37,33 @@ export class HomeComponent  {
     });
   }
 
-  ngOnInit(): void {
-    // You can put any logic here that runs on initialization
+   ngOnInit() {
+    this.wp.getPosts().subscribe((data: any) => {
+      this.posts = data; 
+       this.posts = data.map((post: any) => {
+      return {
+        ...post,
+        courseImage: post.acf?.postimage || '',
+        featuredImage:
+          post._embedded?.['wp:featuredmedia']?.[0]?.source_url || '',
+      };
+    });
+    });
+
+ 
+   
   }
+getShortContent(htmlContent: string, wordLimit: number = 30): string {
+  if (!htmlContent) return '';
+
+  // HTML tags hatao
+  const text = htmlContent.replace(/<[^>]+>/g, '');
+
+  // Words me split karke limit lagao
+  const words = text.split(/\s+/).slice(0, wordLimit);
+
+  return words.join(' ') + (words.length >= wordLimit ? '...' : '');
+}
 
  
 
@@ -47,5 +74,6 @@ export class HomeComponent  {
   onWindowScroll(): void {
     // Optional: Add scroll animation/logic here if needed
   }
+  
 
 }

bd753d1493ce822e51268e165f879df8eceef6cc
nirbhayrana123 <nirbhydesign@gmail.com>
2025-08-19 19:32:33 +0530

    commit amit

--- src/app/layout/book-session/book-session.component.ts
+++ src/app/layout/book-session/book-session.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './book-session.component.html',
   styleUrls: ['./book-session.component.css']   
 })
-export class BookSessionComponent {
+export class BookSessionComponent  implements OnInit {
   constructor(private titleService: Title, private metaService: Meta) {
     this.titleService.setTitle('Book a Session');
     this.metaService.updateTag({
@@ -17,4 +17,14 @@ export class BookSessionComponent {
       content: '',
     });
   }
+
+ ngOnInit(): void {
+    // Calendly script load karna
+    const script = document.createElement('script');
+    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+
 }

b42642b7b7c8d94e4619e2cb84d409efc831449b
nirbhayrana123 <nirbhydesign@gmail.com>
2025-10-01 10:49:41 +0530

    404

--- src/app/layout/not-found/not-found.component.ts
+++ src/app/layout/not-found/not-found.component.ts
@@ -1,24 +1,24 @@
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { StatusCodeService } from '../../services/status-code.service';
 import { isPlatformServer } from '@angular/common';
-
+import { StatusCodeService } from '../../services/status-code.service';
 
 @Component({
-    selector: 'app-not-found',
-    imports: [RouterModule],
-    templateUrl: './not-found.component.html',
-    styleUrls: ['./not-found.component.css']
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent {
-constructor(private meta: Meta, private status: StatusCodeService) {
-
-
-}
+  constructor(
+    private meta: Meta,
+    private status: StatusCodeService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngOnInit(): void {
-     this.status.setStatus(404); 
-    this.meta.updateTag({ name: 'robots', content: 'noindex, nofollow' });
+    if (isPlatformServer(this.platformId)) {
+      this.status.setStatus(404); // 👈 sirf SSR ke liye
+      this.meta.updateTag({ name: 'robots', content: 'noindex, nofollow' });
+    }
   }
 }
